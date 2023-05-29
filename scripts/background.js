18eaed225142ee6a0061be848797f611e0ba5a16
December G <dgarnetsmith@gmail.com>
2023-05-29 04:02:34 -0500

    Possibly create working demo

--- scripts/background.js
+++ scripts/background.js
@@ -9,17 +9,41 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     tabUrl.includes('medium.com')
   ) {
     try {
-      // chrome.tabs.sendMessage(tabId, {"onMedium": true, "error": null})
-        const allCookies = getCookies();
-        console.log(allCookies)
+      const allCookies = getCookies();
+      console.log(allCookies, 'cookies before');
+      // deleteCookies(allCookies);
+      chrome.tabs.sendMessage(tabId, { onMedium: true, error: null });
     } catch (error) {
-      // chrome.tabs.sendMessage(tabId, {"onMedium": false, "error": error})
+      chrome.tabs.sendMessage(tabId, {
+        onMedium: false,
+        error: `The following error has appeared: ${error}`,
+      });
     }
-
   }
 });
 
-    const getCookies = async() => {
-      const cookies = await chrome.cookies.getAll({details: {"name": '_dd_s'}});
-      return cookies;
-    }
+const getCookies = async () => {
+  const cookies = await chrome.cookies.getAll({});
+  // const cookies = await chrome.cookies.getAllCookieStores();
+  deleteCookies(cookies);
+  return cookies;
+};
+
+const deleteCookies = (totalCookies) => {
+  console.log('in delete cookies');
+  console.log('cookies', totalCookies);
+  if (totalCookies.length) {
+    totalCookies.forEach((cookie) => {
+      console.log(cookie, 'cookie');
+      const currentDomain =
+        cookie.domain[0] === '.' ? `www${cookie.domain}` : cookie.domain;
+      console.log('cookie domain', cookie.domain[0]);
+      const cookieUrl = `${
+        cookie.secure ? 'https://' : 'http://'
+      }${currentDomain}${cookie.path}`;
+      console.log(cookieUrl);
+      chrome.cookies.remove({ name: cookie.name, url: cookieUrl });
+    });
+  }
+  // return totalCookies;
+};
--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,9 @@
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
-      const { onMedium, error } = obj;
-      if (response && onMedium) {
-        // localStorage.clear();
-        // console.log('local storage cleared')
-        const allCookies = getCookies();
-        console.log(allCookies)
-      } else {
-        console.error(error)
-      }
-    });
-
+  const { onMedium, error } = obj;
+  if (response && onMedium) {
+    localStorage.clear();
+    console.log('local storage cleared');
+  } else {
+    console.error(error);
+  }
+});
