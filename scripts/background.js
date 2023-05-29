959c134881c16bf3cc23da0122e55bce94ede42d
December G <dgarnetsmith@gmail.com>
2023-05-29 10:36:25 -0500

    Remove console logs and dead code

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   let tabUrl = tab.url ?? tab.pendingUrl;
-  // let fullUrl = tabUrl.split('?')
-  // const queryParams = fullUrl[1]
-  // let domainOnlyUrl = fullUrl[0];
   if (
     changeInfo.status === 'complete' &&
     tabUrl &&
     tabUrl.includes('medium.com')
   ) {
     try {
-      const allCookies = getCookies();
-      console.log(allCookies, 'cookies before');
-      // deleteCookies(allCookies);
+      getCookies();
       chrome.tabs.sendMessage(tabId, { onMedium: true, error: null });
     } catch (error) {
       chrome.tabs.sendMessage(tabId, {
@@ -24,26 +19,19 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 const getCookies = async () => {
   const cookies = await chrome.cookies.getAll({});
-  // const cookies = await chrome.cookies.getAllCookieStores();
   deleteCookies(cookies);
   return cookies;
 };
 
 const deleteCookies = (totalCookies) => {
-  console.log('in delete cookies');
-  console.log('cookies', totalCookies);
   if (totalCookies.length) {
     totalCookies.forEach((cookie) => {
-      console.log(cookie, 'cookie');
       const currentDomain =
         cookie.domain[0] === '.' ? `www${cookie.domain}` : cookie.domain;
-      console.log('cookie domain', cookie.domain[0]);
       const cookieUrl = `${
         cookie.secure ? 'https://' : 'http://'
       }${currentDomain}${cookie.path}`;
-      console.log(cookieUrl);
       chrome.cookies.remove({ name: cookie.name, url: cookieUrl });
     });
   }
-  // return totalCookies;
 };
--- scripts/content.js
+++ scripts/content.js
@@ -2,7 +2,6 @@ chrome.runtime.onMessage.addListener((obj, sender, response) => {
   const { onMedium, error } = obj;
   if (response && onMedium) {
     localStorage.clear();
-    console.log('local storage cleared');
   } else {
     console.error(error);
   }
